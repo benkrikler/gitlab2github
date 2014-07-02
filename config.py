ac36d6a75e911ac8f3116e2ee89b5c3e5b583ef3
mchelem <michele.silva@gmail.com>
2014-07-02 13:54:57 -0300

    Add config file, to specify urls and api keys

---
+++ config.py
@@ -0,0 +1,13 @@
+gitlab_url = '<https://your.gitlab.url>'
+gitlab_api_token = '<your api token>'
+
+github_user = '<your user>'
+github_api_token = '<your key>'
+
+# Used in case usernames for gitlab and github are different
+# The same username is used, if not specified here
+usernames_map = {
+    'gfuhr': 'gustavofuhr',
+    'filipe': 'filipespeck',
+    'bravo': None,
+}
